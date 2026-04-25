36cbb244d0779dfaee6fb2784bd8fd8bcad92918
Oskar Budzisz <oskar.budzisz4@gmail.com>
2026-04-26 00:38:41 +0100

    fix: fix tests post refactor

--- src/test/kotlin/scot/oskar/hytaledevelopment/VersionCheckerTests.kt
+++ src/test/kotlin/scot/oskar/hytaledevelopment/VersionCheckerTests.kt
@@ -1,7 +1,7 @@
 package scot.oskar.hytaledevelopment
 
-import junit.framework.TestCase.assertNotNull
 import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertTrue
 import org.junit.Test
 import scot.oskar.hytaledevelopment.util.HytalePatchline
 import scot.oskar.hytaledevelopment.util.VersionChecker
@@ -10,8 +10,8 @@ class VersionCheckerTests {
 
     @Test
     fun `should return pre-release versions`() = runBlocking {
-        val versions = VersionChecker.fetchVersions(HytalePatchline.PRE_RELEASE)
-        println(versions)
-        assertNotNull(versions)
+        val versionChecker = VersionChecker()
+        val fetchVersions = versionChecker.fetchVersions(HytalePatchline.RELEASE)
+        assertTrue(fetchVersions.isSuccess)
     }
 }
